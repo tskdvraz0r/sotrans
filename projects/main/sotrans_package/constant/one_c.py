bdcb7af0c28daa50481a506adcd3cf90aa1591f3
ts.kdv.raz0r <ts.kdv.raz0r@ya.ru>
2024-01-22 16:29:48 +0300

    [SP] feat: Константа DOC_ALIAS преобразована в функцию get_doc_alias; реализована функция get_doc_names

--- projects/main/sotrans_package/constant/one_c.py
+++ projects/main/sotrans_package/constant/one_c.py
@@ -1,5 +1,74 @@
-DOC_ALIAS: dict[str: str] = {"start_balance": "stb",
-                             "income_balance": "inb",
-                             "expend_balance": "exb",
-                             "end_balance": "enb",
-                             "transit_balance": "trb"}
+def get_doc_alias() -> dict[str,str]:
+    """
+    Notes:
+        Функция возвращает словать "тип документа: алиас документа".
+
+    Returns:
+        (dict[str,str]): Словарь с типами и алиасами документов 1С.
+    """
+
+    return {
+        "start_balance": "stb",
+        "income_balance": "inb",
+        "expend_balance": "exb",
+        "end_balance": "enb",
+        "transit_balance": "trb",
+        "loss_demand": "ldb"
+    }
+
+
+def get_doc_names() -> dict[str, dict[str, str]]:
+    """
+    Notes:
+        Функция принимает на вход тип документа и возвращает словарь {рус: англ} наименований.
+
+    Returns:
+        (dict[str, dict[str, str]]): Словарь {рус: англ} наименований.
+    """
+
+    return {
+        # Start balance
+        "start_balance": {
+            "начальный остаток": "start_balance"
+        },
+
+        # Income Balance
+        "income_balance": {
+            "ввод остатков товаров": "product_entering_balances",
+            "перемещение товаров": "product_movement",
+            "поступление товаров": "product_receipt",
+            "пересортица товаров": "product_resort",
+            "возврат товаров поставщику": "product_return_to_dealer",
+            "инвентаризация товаров": "product_inventory",
+            "поступление дополнительных расходов": "receipt_of_additional_expenses",
+            "разукомплектация": "bundle_separation",
+            "комплектация": "bundle",
+            "корректировка": "income_correction",
+            "корректировка поступления": "income_correction"
+        },
+
+        # Expend balance
+        "expend_balance": {
+            "перемещение товаров": "product_movement",
+            "реализация товаров": "product_sale",
+            "возврат от покупателя": "product_return_from_customer",
+            "пересортица товаров": "product_resort",
+            "списание товаров": "product_writeoff",
+            "закрытие кассовой смены": "close_cash_register",
+            "инвентаризация товаров": "product_inventory",
+            "разукомплектация": "bundle_separation",
+            "комплектация": "bundle",
+            "корректировка реализации": "product_sale_inplementation",
+            "корректировка": "product_sale_inplementation"
+        },
+
+        # End balance
+        "end_balance": {
+            "конечный остаток": "end_balance"
+        },
+
+        # Transit balance
+        "transit_balance": {
+            "товары в пути": "transit_balance"
+        }
+    }
