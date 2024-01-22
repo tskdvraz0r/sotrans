a85a01d3be96f202ab24498128c92291d9fd797e
ts.kdv.raz0r <ts.kdv.raz0r@ya.ru>
2024-01-22 16:35:44 +0300

    [SP] refact: Константы заменены функциями

--- projects/main/sotrans_package/database/postgresql.py
+++ projects/main/sotrans_package/database/postgresql.py
@@ -4,7 +4,7 @@
 # Extended packages
 
 # Sotrans packages
-from .._settings import postgresql
+from .._settings.database import postgresql
 from projects.main.sotrans_package import data_validation
 
 
@@ -15,14 +15,14 @@ def get_database_url(db_name: str) -> str:
         Функция принимает на вход название базы данных в postgres и возвращает ссылку для подключения.
     
     Args:
-        db_name (str): Наименование базы данных. ("tensor", "monitoring", "coefficient", "classifier")
+        db_name (str): Наименование базы данных. ("one_c", "lookup")
     
     Raises:
         TypeError(f'Аргумент "{var_name}" ожидает тип данных "{var_type}".')
         ValueError(f'Значение переменной "{name_arg}" вне списка {include_in}.')
         
     Returns:
-        (str): Функция возвращает строковую ссылку на базу данных "tensor" в Postgres.
+        (str): Функция возвращает строковую ссылку на базу данных в Postgres.
     """
 
     # >>> DATA VALIDATION
@@ -35,8 +35,8 @@ def get_database_url(db_name: str) -> str:
     # Проверить, что значение "db_name" находится в списке.
     data_validation.check.value_in_range(
         value = db_name,
-        in_range = postgresql.DATABASES
+        in_range = postgresql._get_database_names()
     )
 
     # >>> MAIN ALGORITHM
-    return f"postgresql://{postgresql.LOGIN}:{postgresql.PASSWORD}@localhost:5432/{db_name}"
+    return f"postgresql://{postgresql._get_login()}:{postgresql._get_password()}@localhost:5432/{db_name}"
