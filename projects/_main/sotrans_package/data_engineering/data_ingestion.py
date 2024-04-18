44742f3bc49b070d9195388649fccdb2fccf8df5
ts.kdv.raz0r <ts.kdv.raz0r@ya.ru>
2024-04-18 14:14:15 +0300

    [SP] add: Добавлена структура и docstrings

---
+++ projects/_main/sotrans_package/data_engineering/data_ingestion.py
@@ -0,0 +1,47 @@
+class DataIngestion:
+    """
+    Notes:
+        Data Ingestion (потребление данных): перемещение данных из множества разных
+        источников — баз данных SQL и NoSQL, IoT-устройств, веб-сайтов, потоковых
+        сервисов и так далее — в целевую систему с целью преобразования для
+        дальнейшего анализа. Данные поступают в различных видах и могут быть как
+        структурированными, так и неструктурированными.
+    
+    Attributes:
+        pass
+    
+    Methods:
+        pass
+
+    SubClasses:
+        PartMart - методы получения данных из базы данных сайта part-mart.ru;
+        OneC - методы получения данных из отчётов 1С;
+    """
+    
+    class PartMart:
+        """
+        Notes:
+            pass
+        
+        Attributes:
+            pass
+        
+        Methods:
+            pass
+        """
+        
+        pass
+    
+    class OneC:
+        """
+        Notes:
+            pass
+        
+        Attributes:
+            pass
+        
+        Methods:
+            pass
+        """
+        
+        pass
---
+++ projects/_main/sotrans_package/data_engineering/data_serving.py
@@ -0,0 +1,14 @@
+class DataServing:
+    """
+    Notes:
+        Data Serving (передача данных): доставка преобразованных данных конечным 
+        пользователям — платформе BI, в дэшборд или в data analysis/science.
+
+    Attributes:
+        pass
+
+    Methods:
+        pass
+    """
+    
+    pass
