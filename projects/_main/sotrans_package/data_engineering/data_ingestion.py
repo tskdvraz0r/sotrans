721d12fed9a7ec970ea7729bee1f3fe84d1cc282
ts.kdv.raz0r <ts.kdv.raz0r@ya.ru>
2024-04-19 17:35:37 +0300

    [SP] feat: Добавлен метод экстракции данных по брендам из документа 1С

--- projects/_main/sotrans_package/data_engineering/data_ingestion.py
+++ projects/_main/sotrans_package/data_engineering/data_ingestion.py
@@ -1,3 +1,13 @@
+import sys
+
+import pandas as pd
+import sqlalchemy as sa
+
+sys.path.insert(0, r"C:\Users\d.zakharchenko\Desktop\new_structure\sotrans\projects\_main\sotrans_package")
+from _settings import mysql
+from data_analysis.data_validation import DataValidation
+
+
 class DataIngestion:
     """
     Notes:
@@ -44,4 +54,86 @@ class DataIngestion:
             pass
         """
         
-        pass
+        class BatchMovement:
+            """
+            Notes:
+                pass
+            
+            Attributes:
+                pass
+            
+            Methods:
+                pass
+            """
+            
+            @staticmethod
+            def extract_brand(
+                    dataframe: pd.DataFrame
+            ) -> None:
+                """
+                Notes:
+                    Функция принимает на вход датафрейм одного из типов отчётов 1С и вытаскивает 
+                    данные по брендам.
+
+                Attributes:
+                    SQL_BRAND_CONNECTION: Подключение к базе данных.
+                    SQL_SCHEMA: Название схемы.
+                    SQL_TABLE_NAME: Название таблицы.
+                
+                Args:
+                    dataframe (pd.DataFrame): Датафрейм типа:
+                        - Начальный остаток
+                        - Приход
+                        - Расход
+                        - Конечный остаток
+                        - Товары в пути
+                """
+                
+                # ATTRIBUTES
+                SQL_BRAND_CONNECTION: sa.Connection = sa.create_engine(
+                    url=f"mysql+pymysql://{mysql.LOGIN}:{mysql.PASSWORD}@localhost/brand"
+                ).connect()
+                SQL_SCHEMA: str = "brand"
+                SQL_TABLE_NAME: str = "brand_name"
+                
+                # DATA VALIDATION
+                DataValidation.value_type(
+                    value=dataframe,
+                    expected_type=pd.DataFrame
+                )
+                
+                # Сформировать датафрейм с уникальными наименованиями;
+                df_brands: pd.DataFrame = (
+                    pd.DataFrame(data=dataframe["brand_name"])
+                    .sort_values(
+                        by="brand_name",
+                        ascending=True
+                    )
+                    .drop_duplicates(
+                        subset="brand_name",
+                        keep="first",
+                        ignore_index=True
+                    )
+                )
+                
+                # Загрузить данные из MySQL;
+                sql_brand: pd.DataFrame = pd.read_sql_table(
+                    table_name=SQL_TABLE_NAME,
+                    con=SQL_BRAND_CONNECTION,
+                    schema=SQL_SCHEMA
+                )
+                
+                # Оставить только те бренды, который нет в MySQL;
+                df_brands: pd.DataFrame = (
+                    df_brands[~df_brands["brand_name"].isin(values=sql_brand["brand_name"])]
+                )
+                
+                # Загрузить новые данные в MySQL;
+                df_brands.to_sql(
+                    name=SQL_TABLE_NAME,
+                    con=SQL_BRAND_CONNECTION,
+                    schema=SQL_SCHEMA,
+                    if_exists="append",
+                    index=False,
+                    chunksize=100
+                )
