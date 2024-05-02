eb1cc066c2ff785c69f5acd2c96b4609c1a4f462
ts.kdv.raz0r <ts.kdv.raz0r@ya.ru>
2024-05-02 18:10:18 +0300

    [SP] fix: Работа над стилем кода;

--- projects/_main/sotrans_package/data_engineering/data_transformation.py
+++ projects/_main/sotrans_package/data_engineering/data_transformation.py
@@ -2,7 +2,10 @@ import sys
 import string
 import pandas as pd
 
-sys.path.insert(0, r"C:\Users\d.zakharchenko\Desktop\new_structure\sotrans\projects\_main\sotrans_package")
+sys.path.insert(
+    0,
+    r"C:\Users\d.zakharchenko\Desktop\new_structure\sotrans\projects\_main\sotrans_package",
+)
 from _settings._constant._shop import Shop
 from _settings._constant._dealer import Dealer
 from _settings._constant._brand import Brand
@@ -29,7 +32,7 @@ class DataTransformation:
         Notes:
             Класс обработки ошибок в записях.
         """
-        
+
         class Contradiction:
             """
             Notes:
@@ -43,8 +46,7 @@ class DataTransformation:
             """
 
             pass
-        
-        
+
         class Duplicate:
             """
             Notes:
@@ -58,8 +60,7 @@ class DataTransformation:
             """
 
             pass
-        
-        
+
         class Unique:
             """
             Notes:
@@ -71,17 +72,15 @@ class DataTransformation:
             Methods:
                 pass
             """
-            
+
             pass
 
-    
     class Feature:
         """
         Notes:
             Класс обработки ошибок в признаках.
         """
-        
-        
+
         class Abnormal:
             """
             Notes:
@@ -93,10 +92,9 @@ class DataTransformation:
             Methods:
                 None
             """
-            
+
             pass
-        
-        
+
         class ErrorsTypos:
             """
             Notes:
@@ -108,10 +106,9 @@ class DataTransformation:
             Methods:
                 pass
             """
-            
+
             pass
-        
-        
+
         class Invalid:
             """
             Notes:
@@ -123,116 +120,150 @@ class DataTransformation:
             Methods:
                 pass
             """
-            
-            
+
             @staticmethod
-            def clear_dealer(string_to_replace: str) -> str:
+            def clear_dealer_name(series: pd.Series) -> str:
                 """
                 Notes:
-                    Метод принимает на вход строку с наименованием поставщика и очищает её от 
-                    "недопустимых" подстрок.
+                    Метод переименовывает некорректные наименования поставщиков.
 
-                Args:
-                    string_to_replace (str): Наименование поставщика.
-                
                 Raises:
                     TypeError: f"Тип переданных данных не соответствует ожидаемому: {expected_type}")
 
                 Returns:
                     str: Наименование поставщика, очищенное от "недопустимых" подстрок.
                 """
-                
-                # DATA VALIDATION
-                DataValidation.value_type(
-                    value=string_to_replace,
-                    expected_type=str
+
+                # ? DATA VALIDATION
+                (
+                    DataValidation
+                    .value_type(
+                        value=series,
+                        expected_type=pd.Series
+                    )
                 )
-                
-                # MAIN ALGORITHM
-                for false_string, true_string in Dealer.get_contragent_substring_to_replace().items():
+
+                # ! MAIN ALGORITHM
+                series = (
+                    series
+                    .replace(
+                        to_replace=Dealer.get_contragent_name_to_replace()
+                    )
+                )
+
+                # Возврат значения;
+                return series
+
+            @staticmethod
+            def clear_dealer_substring(string_to_replace: str) -> str:
+                """
+                Notes:
+                    Метод принимает на вход строку с наименованием поставщика и очищает её от "недопустимых" подстрок.
+
+                Args:
+                    string_to_replace (str): Наименование поставщика.
+
+                Raises:
+                    TypeError: f"Тип переданных данных не соответствует ожидаемому: {expected_type}")
+
+                Returns:
+                    str: Наименование поставщика, очищенное от "недопустимых" подстрок.
+                """
+
+                # ? DATA VALIDATION
+                (
+                    DataValidation
+                    .value_type(
+                        value=string_to_replace,
+                        expected_type=str
+                    )
+                )
+
+                # ! MAIN ALGORITHM
+                for false_string, true_string in (
+                    Dealer
+                    .get_contragent_substring_to_replace()
+                    .items()
+                ):
                     string_to_replace: str = (
                         string_to_replace
                         .replace(false_string, true_string)
                         .strip()
                     )
-                
+
                 # Возврат значения;
                 return string_to_replace
-            
-            
+
             @staticmethod
             def clear_brand(string_to_replace: str) -> str:
                 """
                 Notes:
-                    Метод принимает на вход строку с наименованием бренда и очищает её от 
+                    Метод принимает на вход строку с наименованием бренда и очищает её от
                     "недопустимых" подстрок.
 
                 Args:
                     string_to_replace (str): Наименование бренда.
-                
+
                 Raises:
                     TypeError: f"Тип переданных данных не соответствует ожидаемому: {expected_type}")
 
                 Returns:
                     str: Наименование бренда, очищенное от "недопустимых" подстрок.
                 """
-                
-                # DATA VALIDATION
-                DataValidation.value_type(
-                    value=string_to_replace,
-                    expected_type=str
+
+                # ? DATA VALIDATION
+                (
+                    DataValidation
+                    .value_type(
+                        value=string_to_replace,
+                        expected_type=str
+                    )
                 )
-                
-                # MAIN ALGORITHM
-                for false_string, true_string in Brand.get_brand_substring_to_replace().items():
+
+                # ! MAIN ALGORITHM
+                for (
+                    false_string,
+                    true_string,
+                ) in Brand.get_brand_name_to_replace().items():
                     string_to_replace: str = (
                         string_to_replace
                         .replace(false_string, true_string)
                         .strip()
                     )
-                
+
                 # Возврат значения;
                 return string_to_replace
-            
-            
+
             @staticmethod
-            def clear_shop(
-                    shop_series: pd.Series
-            ) -> pd.Series:
+            def clear_shop(shop_series: pd.Series) -> pd.Series:
                 """
                 Notes:
-                    Метод принимает на вход pd.Series с наименованием магазинов и приводит к 
+                    Метод принимает на вход pd.Series с наименованием магазинов и приводит к
                     требуемому виду путём переименования и "объединения" магазинов/складов.
 
                 Args:
                     shop_series (pd.Series): Массив с наименованиями складов/магазинов.
-                    
+
                 Raises:
                     TypeError: f"Тип переданных данных не соответствует ожидаемому: {expected_type}")
 
                 Returns:
                     (pd.Series): Массив с переименованными складами/магазинами.
                 """
-                
+
                 # DATA VALIDATION
-                DataValidation.value_type(
-                    value=shop_series,
-                    expected_type=pd.Series
-                )
-                
+                DataValidation.value_type(value=shop_series, expected_type=pd.Series)
+
                 # MAIN ALGORITHM
-                shop_series = (
-                    shop_series
-                    .replace(to_replace=Shop.get_shop_substring_to_replace())
+                shop_series = shop_series.replace(
+                    to_replace=Shop.get_shop_substring_to_replace()
                 )
-                
+
                 return shop_series
-            
-            
+
             @staticmethod
             def clear_product_catalog_number(
-                    series: pd.Series,
-                    clearing_method: str
+                series: pd.Series, clearing_method: str
             ) -> pd.Series:
                 """
                 Notes:
@@ -244,7 +275,7 @@ class DataTransformation:
                         - sotrans - очистка от символов пунктуации с предварительным удалением алиаса бренда "_brand".
 
                 Args:
-                    series (pd.Series): Pandas серия, в которой требуется очистить значения от 
+                    series (pd.Series): Pandas серия, в которой требуется очистить значения от
                     символов пунктуации.
                     clearing_method (str): Метод очистки: "origin" | "sotrans".
 
@@ -254,62 +285,51 @@ class DataTransformation:
                 Returns:
                     (pd.DataFrame): Датафрейм со столбцом "product_article_number" (очищенным номером по каталогу).
                 """
-                
+
                 # DATA VALIDATION
                 for value, expected_type in zip(
-                        (series, clearing_method),
-                        (pd.Series, str)
+                    (series, clearing_method), (pd.Series, str)
                 ):
-                    DataValidation.value_type(
-                        value=value,
-                        expected_type=expected_type
-                    )
-                
+                    DataValidation.value_type(value=value, expected_type=expected_type)
+
                 # FUNCTIONS
-                def origin_method(
-                        product_article_number: str
-                ) -> str:
+                def origin_method(product_article_number: str) -> str:
                     """
                     Notes:
-                        Метод принимает на вход номер по каталогу/артикул и производит приведение 
+                        Метод принимает на вход номер по каталогу/артикул и производит приведение
                         к верхнему регистру и очистку от знаков пунктуации.
 
                     Args:
                         product_catalog_number (str): Номер по каталогу/артикул.
-                    
+
                     Raises:
                         TypeError: f"Тип переданныых данных не соответствует ожидаемому: {expected_type}")
 
                     Returns:
                         str: Номер, очищенный от знаков пунктуации.
                     """
-                    
+
                     # DATA VALIDATION
                     DataValidation.value_type(
-                        value=product_article_number,
-                        expected_type=str
+                        value=product_article_number, expected_type=str
                     )
 
                     # MAIN ALGORITHM
                     # Очистить от символов пунктуации;
                     for symbol in f"{string.punctuation} ":
-                        product_article_number: str = (
-                            product_article_number
-                            .replace(symbol, "")
-                            .strip()
-                        )
+                        product_article_number: str = product_article_number.replace(
+                            symbol, ""
+                        ).strip()
 
                     # Возврат результата;
                     return product_article_number
 
-                def sotrans_method(
-                        product_catalog_number: str
-                ) -> str:
+                def sotrans_method(product_catalog_number: str) -> str:
                     """
                     Notes:
-                        Метод принимает на вход номер по каталогу. Производит приведение 
+                        Метод принимает на вход номер по каталогу. Производит приведение
                         к верхнему регистру, очистку от знаков пунктуации и алиаса бренда.
-                    
+
                     Raises:
                         TypeError: f"Тип переданныых данных не соответствует ожидаемому: {expected_type}")
 
@@ -319,26 +339,24 @@ class DataTransformation:
                     Returns:
                         str: Номер, очищенный от знаков пунктуации.
                     """
-                    
+
                     # DATA VALIDATION
                     DataValidation.value_type(
-                        value=product_catalog_number,
-                        expected_type=str
+                        value=product_catalog_number, expected_type=str
                     )
 
                     # MAIN ALGORITHM
                     # Очистить от алиаса бренда и символов пунктуации;
                     for symbol in f"{string.punctuation} ":
                         product_catalog_number: str = (
-                            product_catalog_number
-                            .split("_", -1)[0]
+                            product_catalog_number.split("_", -1)[0]
                             .replace(symbol, "")
                             .strip()
                         )
 
                     # Возврат результата;
                     return product_catalog_number
-                
+
                 # MAIN ALGORITHM
                 match clearing_method:
                     case "origin":
@@ -348,7 +366,7 @@ class DataTransformation:
                                 for value in series
                             ]
                         )
-                    
+
                     case "sotrans":
                         series = pd.Series(
                             data=[
@@ -359,7 +377,7 @@ class DataTransformation:
 
                 # Возврат результата;
                 return series
-        
+
         class Missed:
             """
             Notes:
@@ -371,10 +389,9 @@ class DataTransformation:
             Methods:
                 pass
             """
-            
+
             pass
-        
-        
+
         class Polysemy:
             """
             Notes:
@@ -389,5 +406,5 @@ class DataTransformation:
             Methods:
                 pass
             """
-            
+
             pass
